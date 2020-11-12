ed02317e762eb8e62a6418cc78c9a84c2d89c7b1
Marko Lalovic <marko@markolalovic.com>
2020-11-12 21:19:30 +0100

    generated all the glyphs

---
+++ normal-fonts.r
@@ -0,0 +1,33 @@
+# normal-fonts.r - Draw the letters as contours of bivariate normal density
+
+library("mvtnorm")
+
+#' Saves pictures of all the characters
+save <- function(letter, d, s1, a, b, n,
+  N=500, s2=10, lwd=5, drawlabels=FALSE) {
+    x <- seq(a, b, length = N)
+    levs <- pretty(d, n=n)
+    levs <- levs[s1:length(levs)]
+    fname <- paste("glyphs/", letter, ".png", sep="")
+    png(file=fname, width=2000, height=2000)
+    contour(x, x, d,
+            levels=levs[-c(s2, length(levs))],
+            col = "black", # 1:4 - rainbow style
+            lwd = lwd,
+            lty = 1,
+            drawlabels = drawlabels,
+            bty = "n",
+            xaxt = "n",
+            yaxt = "n")
+    dev.off()
+}
+
+letters <- 1:26 # the English alphabet consists of 26 letters:
+names(letters) <- c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+  "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+paramsList <- readRDS(file = "paramsList.rds") # letter, d - matrix, s1, a, b, n
+for (i in letters) {
+    params <- paramsList[[i]]
+    save(params$letter, params$d, params$s1, params$a, params$b, params$n)
+}
